b46a5b9976236224b216d826d2b2fc5b03a976b2
anthony-ly <director2911@gmail.com>
2022-04-15 14:27:03 +1000

    Minor code refactor.

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import AppButton from './app/components/AppButton';
-import AppColors from './app/config/AppColors';
-import WelcomeScreen from './app/screens/WelcomeScreen';
 import AuthNavigator from './app/navigation/AuthNavigator';
 
 
@@ -14,8 +11,6 @@ export default function App() {
     <NavigationContainer>
       <AuthNavigator />
     </NavigationContainer>
-    // <LoginScreen />
-    // <RegisterScreen />
 
   );
 }

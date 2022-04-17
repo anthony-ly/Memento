5eb1d3f95464cf9a674f8a698e8585ee9914677e
anthony-ly <director2911@gmail.com>
2022-04-17 14:56:45 +1000

    Added FlatList code.

--- app/screens/MemoryScreen.js
+++ app/screens/MemoryScreen.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 import AppCard from '../components/AppCard';
 import AppScreen from '../components/AppScreen';
 import AppColors from '../config/AppColors';
 
+const memoryData = [
+    {
+        id: 1,
+        title: "deez nuts",
+        subtitle: "rick roll",
+    },
+    {
+        id: 2,
+        title: "joe mama",
+        subtitle: "ok"
+    }
+]
+
 function MemoryScreen(props) {
     return (
         <AppScreen>
-            <AppCard
+            {/* <AppCard
                 title="deez"
-                subtitle="nuts" />
+                subtitle="nuts" /> */}
+            <FlatList
+                data={memoryData}
+                keyExtractor={memory => memory.id.toString()}
+                renderItem={
+                    ({ item }) => <AppCard title={item.title} subtitle={item.subtitle} />} />
+
         </AppScreen>
     );
 }

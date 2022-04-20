e50d02950418ffe3fc3a123a837b680e4d59c61d
anthony-ly <director2911@gmail.com>
2022-04-20 16:14:25 +1000

    Added error handling for New Memory Screen.

--- app/screens/NewMemoryScreen.js
+++ app/screens/NewMemoryScreen.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+// import * as Yup from 'yup';
+
 import AppPicker from '../components/AppPicker';
 import AppScreen from '../components/AppScreen';
 import AppTextInput from '../components/AppTextInput';
 import AppColors from '../config/AppColors';
+import AppButton from '../components/AppButton';
+import AppText from '../components/AppText';
 
 const categories = [
     { label: "L1", value: 1, icon: "airplane-takeoff", backgroundColor: "red" },
@@ -11,10 +16,31 @@ const categories = [
     { label: "L3", value: 3, icon: "flash", backgroundColor: "green" },
 ];
 
+// const schema = Yup.object().shape(
+//     {
+//         title: Yup.string().required.
+//     }
+// )
+
 function NewMemoryScreen(props) {
+    const [title, setTitle] = useState("");
+    const [subTitle, setSubTitle] = useState("");
     const [category, setCategory] = useState();
-    const [title, setTitle] = useState();
-    const [subTitle, setSubTitle] = useState();
+
+    const [titleError, setTitleError] = useState("");
+    const [subTitleError, setSubTitleError] = useState("");
+    const [categoryError, setCategoryError] = useState("");
+    // const[imageError, setImageError]=useState("");
+
+    const doErrorCheck = () => {
+        setTitleError(title.length > 0 ? "" : "Please set a valid Book Title");
+        setSubTitleError(subTitle.length > 0 ? "" : "Please set a valid subtitle");
+        setCategoryError(category ? "" : "Please pick a category from the list");
+        // setImageError(image ? "" : "Please pick an image");
+        return ((title.length > 0) && (subTitle.length > 0) && (category)
+            // && (image) 
+            ? true : false)
+    }
 
     return (
         <AppScreen style={styles.container}>
@@ -25,12 +51,15 @@ function NewMemoryScreen(props) {
                 onChangeText={(inputText) => setTitle(inputText)}
             />
 
+            {titleError.length > 0 ? <AppText style={{ margin: 5, color: "red", fontSize: 16 }}>{titleError}</AppText> : <></>}
+
             <AppTextInput
                 icon="calendar-month"
                 placeholder="Date"
                 value={subTitle}
                 onChangeText={(inputText) => setSubTitle(inputText)}
             />
+            {subTitleError.length > 0 ? <AppText style={{ margin: 5, color: "red", fontSize: 16 }}>{subTitleError}</AppText> : <></>}
 
             <AppPicker
                 selectedItem={category}
@@ -38,6 +67,9 @@ function NewMemoryScreen(props) {
                 data={categories}
                 icon="apps"
                 placeholder="Categories" />
+            {categoryError.length > 0 ? <AppText style={{ margin: 5, color: "red", fontSize: 16 }}>{categoryError}</AppText> : <></>}
+
+            <AppButton title="Add Memory" color="primaryColor" onPress={() => doErrorCheck()} />
         </AppScreen>
     );
 }
